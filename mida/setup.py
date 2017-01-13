6ec0a74e0decbffc57403819a40f683fe4e8af8a
Tom Watson <tom.watson@1stscreen.com>
2017-01-13 11:21:01 +1100

    Added setup.py to root directory

---
+++ mida/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+with codecs.open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name='mida',
+    version='0.1.0.dev1',
+    description=long_description,
+    url='https://github.com/FridgeSeal/Mida',
+    author='FridgeSeal',
+    author_email='',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers/Science/Research',
+        'License ::  OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6'
+    ],
+    keywords='greatcircledistance',
+    packages=setuptools.find_packages('greatcircle'),
+    install_requires=['numba']
+)
